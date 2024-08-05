53ef09a52fc44f8d6057ecffcc48fad24e9542ec
Emmankoko <emmankoko519@gmail.com>
2024-08-05 16:54:19 +0000

    implement device interface for codel

--- sys/altq/altq_codel.c
+++ sys/altq/altq_codel.c
@@ -26,7 +26,7 @@
  * provided "as is" without express or implied warranty of any kind.
  *
  * These notices must be retained in any copies of any part of this software.
- * 
+ *
  * CoDel - The Controlled-Delay Active Queue Management algorithm
  */
 
@@ -54,6 +54,16 @@
 #include <altq/altq.h>
 #include <altq/altq_codel.h>
 
+/* codel interface state list to keep all codel states allocated*/
+static codel_if *codel_list = NULL;
+
+#define CODEL_LIMIT 1000
+#define DEF_TARG	5
+#define DEF_INT		100
+
+static int default_interval = DEF_INT;
+static int default_target =   DEF_TARG;
+
 static int		 codel_should_drop(struct codel *, class_queue_t *,
 			    struct mbuf *, u_int64_t);
 static void		 codel_Newton_step(struct codel_vars *);
@@ -64,10 +74,13 @@ static u_int64_t	 codel_control_law(u_int64_t t, u_int64_t, u_int32_t);
 #define	codel_time_before(a, b)		((int64_t)(a) - (int64_t)(b) < 0)
 #define	codel_time_before_eq(a, b)	((int64_t)(a) - (int64_t)(b) <= 0)
 
+#ifdef ALTQ3_COMPAT
 static int codel_request(struct ifaltq *, int, void *);
 
 static int codel_enqueue(struct ifaltq *, struct mbuf *, struct altq_pktattr *);
 static struct mbuf *codel_dequeue(struct ifaltq *, int);
+static int codel_detach(struct codel_if *);
+#endif /* ALTQ3_COMPAT */
 
 #if NPF > 0
 int
@@ -175,79 +188,6 @@ codel_getqstats(struct pf_altq *a, void *ubuf, int *nbytes)
 	return (0);
 }
 #endif /* NPF */
-
-static int
-codel_request(struct ifaltq *ifq, int req, void *arg)
-{
-	struct codel_if	*cif = (struct codel_if *)ifq->altq_disc;
-	struct mbuf *m;
-
-	switch (req) {
-	case ALTRQ_PURGE:
-		if (!ALTQ_IS_ENABLED(cif->cif_ifq))
-			break;
-
-		if (qempty(cif->cl_q))
-			break;
-
-		while ((m = _getq(cif->cl_q)) != NULL) {
-			PKTCNTR_ADD(&cif->cl_stats.cl_dropcnt, m_pktlen(m));
-			m_freem(m);
-			IFQ_DEC_LEN(cif->cif_ifq);
-		}
-		cif->cif_ifq->ifq_len = 0;
-		break;
-	}
-
-	return 0;
-}
-
-static int
-codel_enqueue(struct ifaltq *ifq, struct mbuf *m, struct altq_pktattr *pktattr)
-{
-
-	struct codel_if *cif = (struct codel_if *) ifq->altq_disc;
-
-	/* grab class set by classifier */
-	if ((m->m_flags & M_PKTHDR) == 0) {
-		/* should not happen */
-		printf("altq: packet for %s does not have pkthdr\n",
-		   ifq->altq_ifp->if_xname);
-		m_freem(m);
-		PKTCNTR_ADD(&cif->cl_stats.cl_dropcnt, m_pktlen(m));
-		return ENOBUFS;
-	}
-
-	if (codel_addq(&cif->codel, cif->cl_q, m)) {
-		PKTCNTR_ADD(&cif->cl_stats.cl_dropcnt, m_pktlen(m));
-		return ENOBUFS;
-	}
-	IFQ_INC_LEN(ifq);
-
-	return 0;
-}
-
-static struct mbuf *
-codel_dequeue(struct ifaltq *ifq, int op)
-{
-	struct codel_if *cif = (struct codel_if *)ifq->altq_disc;
-	struct mbuf *m;
-
-	if (IFQ_IS_EMPTY(ifq))
-		return NULL;
-
-	if (op == ALTDQ_POLL)
-		return (qhead(cif->cl_q));
-
-	m = codel_getq(&cif->codel, cif->cl_q);
-	if (m != NULL) {
-		IFQ_DEC_LEN(ifq);
-		PKTCNTR_ADD(&cif->cl_stats.cl_xmitcnt, m_pktlen(m));
-		return m;
-	}
-
-	return NULL;
-}
 
 struct codel *
 codel_alloc(int target, int interval, int ecn)
@@ -466,5 +406,268 @@ codel_getstats(struct codel *c, struct codel_stats *s)
 {
 	*s = c->stats;
 }
+
+#ifdef ALTQ3_COMPAT
+
+/*
+ * CoDel device interface
+ */
+altqdev_decl(codel);
+
+int
+codelopen(dev_t dev, int flag, int fmt,
+		struct lwp *l)
+{
+	/*everything will be done the queuing scheme is attached */
+	return 0;
+}
+
+int
+codelclose(dev_t dev, int flag, int fmt,
+		struct lwp *l)
+{
+	struct codel_if *cod;
+	int err, error = 0;
+
+	while ((cod = codel_list) != NULL) {
+		err = codel_detach(cod);
+		if (err != 0 && error == 0)
+			error = err;
+	}
+	return error;
+}
+
+int
+codelioctl(dev_t dev, ioctlcmd_t cmd, void *addr, int flag
+		struct lwp *l )
+{
+	struct codel_if cod;
+	struct codel_interface *ifacep;
+	struct ifnet *ifp;
+	int error = 0;
+
+	switch (cmd) {
+
+		case CODEL_ENABLE:
+			ifacep = (struct codel_interface *)addr;
+			if ((cod = altq_lookup(ifacep->codel_ifname, ALTQT_CODEL)) == NULL) {
+				error = EBADF;
+				break;
+			}
+			error = altq_enable(cod->cif_ifq);
+			break;
+
+		case CODEL_DISABLE:
+			ifacep = (struct codel_interface *)addr;
+			if ((cod = altq_lookup(ifacep->codel_ifname, ALTQT_CODEL)) == NULL) {
+				error = EBADF;
+				break;
+			}
+			error = altq_disable(cod->cif_ifq);
+			break;
+
+		case CODEL_IF_ATTACH:
+			ifp = ifunit(((struct codel_interface *)addr)->codel_ifname);
+			if (ifp == NULL) {
+				error = ENXIO;
+				break;
+			}
+
+			/* allocate and initialize red_queue_t */
+			cod = malloc(sizeof(struct codel_if), M_DEVBUF, M_WAITOK|M_ZERO);
+			if (rqp == NULL) {
+				error = ENOMEM;
+				break;
+			}
+
+			cod->cl_q = malloc(sizeof(class_queue_t), M_DEVBUF,
+				M_WAITOK|M_ZERO);
+			if (cod->cl_q == NULL) {
+				free(cod, M_DEVBUF);
+				error = ENOMEM;
+				break;
+			}
+
+			cod->codel = codel_alloc(0, 0, 0);
+			if (cod->codel == NULL) {
+				free(cod->cl_q, M_DEVBUF);
+				free(cod, M_DEVBUF);
+				error = ENOMEM;
+				break;
+			}
+
+			cod->cif_ifq = &ifp->if_snd;
+			qtail(cod->cl_q) = NULL;
+			qlen(cod->cl_q) = 0;
+			qlimit(cod->cl_q) = CODEL_LIMIT;
+			qtype(cod->cl_q) = Q_CODEL;
+
+			/*
+			* set RED to this ifnet structure.
+			*/
+			error = altq_attach(cod->cif_ifq, ALTQT_CODEL, cod,
+						codel_enqueue, codel_dequeue, codel_request,
+						NULL, NULL);
+			if (error) {
+				codel_destroy(cod->codel);
+				free(cod->cl_q, M_DEVBUF);
+				free(cod, M_DEVBUF);
+				break;
+			}
+
+			/* add this state to the red list */
+			cod->cif_next = cod_list;
+			cod_list = cod;
+			break;
+
+		case CODEL_IF_DETACH:
+			ifacep = (struct codel_interface *)addr;
+			if ((cod = altq_lookup(ifacep->codel_ifname, ALTQT_CODEL)) == NULL) {
+				error = EBADF;
+				break;
+			}
+			error = codel_detach(cod);
+			break;
+
+		case RED_GETSTATS:
+			do {
+				struct codel_ifstats *q_stats;
+				struct codel *cd;
+
+				q_stats = (struct codel_ifstats *)addr;
+				if ((cod = altq_lookup(q_stats->iface.codel_ifname,
+							ALTQT_CODEL)) == NULL) {
+					error = EBADF;
+					break;
+				}
+
+				q_stats->qlength 	= qlen(cod->cl_q);
+				q_stats->qlimit   	= qlimit(cod->cl_q);
+
+				cd = cod->codel;
+
+				q_stats->cl_dropcnt = cd->stats.drop_cnt;
+				q_stats->stats.maxpacket = cd->stats.maxpacket;
+				q_stats->stats.marked_packets = cd->stats.marked_packets;
+
+			} while (/* CONSTCOND */ 0);
+			break;
+
+		case CODEL_SETDEFAULTS:
+			do {
+				struct codel_params cd;
+				cd = (struct codel_params *)addr;
+
+				default_target = cd->target;
+				default_interval = cd->interval;
+			} while (/* CONSTCOND */ 0);
+			break;
+
+		default:
+				error = EINVAL;
+				break;
+		}
+		return error;
+}
+
+static int
+codel_request(struct ifaltq *ifq, int req, void *arg)
+{
+	struct codel_if	*cif = (struct codel_if *)ifq->altq_disc;
+	struct mbuf *m;
+
+	switch (req) {
+	case ALTRQ_PURGE:
+		if (!ALTQ_IS_ENABLED(cif->cif_ifq))
+			break;
+
+		if (qempty(cif->cl_q))
+			break;
+
+		while ((m = _getq(cif->cl_q)) != NULL) {
+			PKTCNTR_ADD(&cif->cl_stats.cl_dropcnt, m_pktlen(m));
+			m_freem(m);
+			IFQ_DEC_LEN(cif->cif_ifq);
+		}
+		cif->cif_ifq->ifq_len = 0;
+		break;
+	}
+
+	return 0;
+}
+
+static int
+codel_enqueue(struct ifaltq *ifq, struct mbuf *m, struct altq_pktattr *pktattr)
+{
+
+	struct codel_if *cif = (struct codel_if *) ifq->altq_disc;
+
+	/* grab class set by classifier */
+	if ((m->m_flags & M_PKTHDR) == 0) {
+		/* should not happen */
+		printf("altq: packet for %s does not have pkthdr\n",
+		   ifq->altq_ifp->if_xname);
+		m_freem(m);
+		PKTCNTR_ADD(&cif->cl_stats.cl_dropcnt, m_pktlen(m));
+		return ENOBUFS;
+	}
+
+	if (codel_addq(&cif->codel, cif->cl_q, m)) {
+		PKTCNTR_ADD(&cif->cl_stats.cl_dropcnt, m_pktlen(m));
+		return ENOBUFS;
+	}
+	IFQ_INC_LEN(ifq);
+
+	return 0;
+}
+
+static struct mbuf *
+codel_dequeue(struct ifaltq *ifq, int op)
+{
+	struct codel_if *cif = (struct codel_if *)ifq->altq_disc;
+	struct mbuf *m;
+
+	if (IFQ_IS_EMPTY(ifq))
+		return NULL;
+
+	if (op == ALTDQ_POLL)
+		return (qhead(cif->cl_q));
+
+	m = codel_getq(&cif->codel, cif->cl_q);
+	if (m != NULL) {
+		IFQ_DEC_LEN(ifq);
+		PKTCNTR_ADD(&cif->cl_stats.cl_xmitcnt, m_pktlen(m));
+		return m;
+	}
+
+	return NULL;
+}
+
+static int
+codel_detach(struct codel_if *cif)
+{
+	struct codel_if *tmp;
+	int error = 0;
+
+	if (ALTQ_IS_ENABLED(cif->cif_ifq))
+		altq_disable(cif->cif_ifq);
+
+	if ((error = altq_detach(cif->cif_ifq)))
+		return (error);
+
+	if (codel_list == cif)
+		codel_list = cif->cif_next;
+	else {
+		for (tmp = codel_list; tmp != NULL; tmp = tmp->cif_next)
+			if (tmp->cif_next == cif) {
+				tmp->cif_next = cif->cif_next;
+				break;
+			}
+		if (tmp == NULL)
+			printf("codel_detach: no state found in red_list!\n");
+	}
+}
+
+#endif /* ALTQ3_COMPAT */
 
 #endif /* ALTQ_CODEL */
