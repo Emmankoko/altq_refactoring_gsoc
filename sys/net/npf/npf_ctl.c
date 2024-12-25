86d2f58c940ce1c0fcc6cd4658cbf95a53d904d8
Emmankoko <emmankoko519@gmail.com>
2024-12-25 00:14:14 +0000

    check tables size

--- sys/net/npf/npf_ctl.c
+++ sys/net/npf/npf_ctl.c
@@ -210,6 +210,7 @@ npf_mk_tables(npf_t *npf, const nvlist_t *req, nvlist_t *resp, npf_config_t *nc)
 	if (nvlist_exists_nvlist_array(req, "tables")) {
 		tables = nvlist_get_nvlist_array(req, "tables", &nitems);
 		if (nitems > NPF_MAX_TABLES) {
+			printf("nitem is ..%d", (int)nitems);
 			NPF_ERR_DEBUG(resp);
 			return E2BIG;
 		}
