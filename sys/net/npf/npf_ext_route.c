d40e4875ffa2998ef1db6afb62154b12989ca644
Emmankoko <emmankoko519@gmail.com>
2025-07-07 10:36:43 +0000

    silly push

--- sys/net/npf/npf_ext_route.c
+++ sys/net/npf/npf_ext_route.c
@@ -132,21 +132,23 @@ npf_route(npf_cache_t *npc, nbuf_t *nbuf, void *meta, int *decision)
 	} else if (npf_iscached(npc, NPC_IP4)) {
 			struct ip *ip = npc->npc_ip.v4;
 			struct mbuf *m = m0;
+
 			sockaddr_in_init(&v4.dst, &ip->ip_dst, 0);
 			sockaddr_in_init(&v4.src, &ip->ip_src, 0);
 
+			/* NB: This code is copied from ip_output */
 			hlen = ip->ip_hl << 2;
-
 			m->m_pkthdr.csum_data |= hlen << 16;
 
-			/* NB: This code is copied from ip_output */
 			if (m->m_pkthdr.csum_flags & (M_CSUM_TCPv4|M_CSUM_UDPv4)) {
 				in_undefer_cksum_tcpudp(m);
 				m->m_pkthdr.csum_flags &= ~(M_CSUM_TCPv4|M_CSUM_UDPv4);
 			}
 			if (ntohs(ip->ip_len) <= ifp->if_mtu) {
 
-				error = ip_if_output(ifp, m, sintocsa(&dst.v4), rt);
+				error = ip_if_output(ifp, m, sintocsa(&dst.v4), NULL);
+				if (error)
+					goto bad;
 				goto done;
 			}
 
@@ -161,7 +163,7 @@ npf_route(npf_cache_t *npc, nbuf_t *nbuf, void *meta, int *decision)
 				goto bad;
 			}
 
-			error = ip_fragment(m, ifp, mtu);
+			error = ip_fragment(m, ifp, ifp->if_mtu);
 			if (error) {
 				m = NULL;
 				goto bad;
@@ -190,189 +192,6 @@ npf_route(npf_cache_t *npc, nbuf_t *nbuf, void *meta, int *decision)
 			/* XXX: statistics */
 			goto done;
 	}
-
-
-
-
-	ip = mtod(m, struct ip *);
-	hlen = ip->ip_hl << 2;
-
-	m->m_pkthdr.csum_data |= hlen << 16;
-
-	/*
-	 * search for the source address structure to
-	 * maintain output statistics, and verify address
-	 * validity
-	 */
-	KASSERT(ia == NULL);
-	sockaddr_in_init(&v4.sin, &ip->ip_src, 0);
-	ia = ifatoia(ifaof_ifpforaddr_psref(&v4.src, ifp, &psref_ia));
-
-	/*
-	 * Ensure we only send from a valid address.
-	 * A NULL address is valid because the packet could be
-	 * generated from a packet filter.
-	 */
-	if (ia != NULL && (flags & IP_FORWARDING) == 0 &&
-	    (error = ip_ifaddrvalid(ia)) != 0)
-	{
-		ARPLOG(LOG_ERR,
-		    "refusing to send from invalid address %s (pid %d)\n",
-		    ARPLOGADDR(&ip->ip_src), curproc->p_pid);
-		IP_STATINC(IP_STAT_ODROPPED);
-		if (error == 1)
-			/*
-			 * Address exists, but is tentative or detached.
-			 * We can't send from it because it's invalid,
-			 * so we drop the packet.
-			 */
-			error = 0;
-		else
-			error = EADDRNOTAVAIL;
-		goto bad;
-	}
-
-	/* Maybe skip checksums on loopback interfaces. */
-	if (IN_NEED_CHECKSUM(ifp, M_CSUM_IPv4)) {
-		m->m_pkthdr.csum_flags |= M_CSUM_IPv4;
-	}
-	sw_csum = m->m_pkthdr.csum_flags & ~ifp->if_csum_flags_tx;
-
-	/* Need to fragment the packet */
-	if (ntohs(ip->ip_len) > mtu &&
-	    (m->m_pkthdr.csum_flags & M_CSUM_TSOv4) == 0) {
-		goto fragment;
-	}
-
-#if IFA_STATS
-	if (ia)
-		ia->ia_ifa.ifa_data.ifad_outbytes += ntohs(ip->ip_len);
-#endif
-	/*
-	 * Always initialize the sum to 0!  Some HW assisted
-	 * checksumming requires this.
-	 */
-	ip->ip_sum = 0;
-
-	if ((m->m_pkthdr.csum_flags & M_CSUM_TSOv4) == 0) {
-		/*
-		 * Perform any checksums that the hardware can't do
-		 * for us.
-		 *
-		 * XXX Does any hardware require the {th,uh}_sum
-		 * XXX fields to be 0?
-		 */
-		if (sw_csum & M_CSUM_IPv4) {
-			KASSERT(IN_NEED_CHECKSUM(ifp, M_CSUM_IPv4));
-			ip->ip_sum = in_cksum(m, hlen);
-			m->m_pkthdr.csum_flags &= ~M_CSUM_IPv4;
-		}
-		if (sw_csum & (M_CSUM_TCPv4|M_CSUM_UDPv4)) {
-			if (IN_NEED_CHECKSUM(ifp,
-			    sw_csum & (M_CSUM_TCPv4|M_CSUM_UDPv4))) {
-				in_undefer_cksum_tcpudp(m);
-			}
-			m->m_pkthdr.csum_flags &=
-			    ~(M_CSUM_TCPv4|M_CSUM_UDPv4);
-		}
-	}
-
-	sa = (m->m_flags & M_MCAST) ? sintocsa(rdst) : sintocsa(dst);
-
-	/* Send it */
-	if (__predict_false(sw_csum & M_CSUM_TSOv4)) {
-		/*
-		 * TSO4 is required by a packet, but disabled for
-		 * the interface.
-		 */
-		error = ip_tso_output(ifp, m, sa, rt);
-	} else
-		error = ip_if_output(ifp, m, sa, rt);
-	goto done;
-
-fragment:
-	/*
-	 * We can't use HW checksumming if we're about to fragment the packet.
-	 *
-	 * XXX Some hardware can do this.
-	 */
-	if (m->m_pkthdr.csum_flags & (M_CSUM_TCPv4|M_CSUM_UDPv4)) {
-		if (IN_NEED_CHECKSUM(ifp,
-		    m->m_pkthdr.csum_flags & (M_CSUM_TCPv4|M_CSUM_UDPv4))) {
-			in_undefer_cksum_tcpudp(m);
-		}
-		m->m_pkthdr.csum_flags &= ~(M_CSUM_TCPv4|M_CSUM_UDPv4);
-	}
-
-	/*
-	 * Too large for interface; fragment if possible.
-	 * Must be able to put at least 8 bytes per fragment.
-	 */
-	if (ntohs(ip->ip_off) & IP_DF) {
-		if (flags & IP_RETURNMTU) {
-			KASSERT(inp != NULL);
-			in4p_errormtu(inp) = mtu;
-		}
-		error = EMSGSIZE;
-		IP_STATINC(IP_STAT_CANTFRAG);
-		goto bad;
-	}
-
-	error = ip_fragment(m, ifp, mtu);
-	if (error) {
-		m = NULL;
-		goto bad;
-	}
-
-	for (; m; m = m0) {
-		m0 = m->m_nextpkt;
-		m->m_nextpkt = NULL;
-		if (error) {
-			m_freem(m);
-			continue;
-		}
-#if IFA_STATS
-		if (ia)
-			ia->ia_ifa.ifa_data.ifad_outbytes += ntohs(ip->ip_len);
-#endif
-		/*
-		 * If we get there, the packet has not been handled by
-		 * IPsec whereas it should have. Now that it has been
-		 * fragmented, re-inject it in ip_output so that IPsec
-		 * processing can occur.
-		 */
-		if (natt_frag) {
-			error = ip_output(m, opt, NULL,
-			    flags | IP_RAWOUTPUT | IP_NOIPNEWID,
-			    imo, inp);
-		} else {
-			KASSERT((m->m_pkthdr.csum_flags &
-			    (M_CSUM_UDPv4 | M_CSUM_TCPv4)) == 0);
-			error = ip_if_output(ifp, m, (m->m_flags & M_MCAST) ?
-			    sintocsa(rdst) : sintocsa(dst), rt);
-		}
-	}
-	if (error == 0) {
-		IP_STATINC(IP_STAT_FRAGMENTED);
-	}
-
-done:
-	ia4_release(ia, &psref_ia);
-	rtcache_unref(rt, ro);
-	if (ro == &iproute) {
-		rtcache_free(&iproute);
-	}
-	if (mifp != NULL) {
-		if_put(mifp, &psref);
-	}
-	if (bind_need_restore)
-		curlwp_bindx(bound);
-	return error;
-
-bad:
-	m_freem(m);
-	goto done;
-}
 
 done:
 	KERNEL_UNLOCK_ONE(NULL);
