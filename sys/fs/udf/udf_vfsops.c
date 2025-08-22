e10948ada5d55776dac4ba98cd465de2f86e573e
Emmankoko <emmankoko519@gmail.com>
2025-08-22 14:01:28 +0000

    null ptr deref

--- sys/fs/udf/udf_vfsops.c
+++ sys/fs/udf/udf_vfsops.c
@@ -3,7 +3,7 @@
 /*
  * Copyright (c) 2006, 2008 Reinoud Zandijk
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -12,7 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -23,7 +23,7 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 #include <sys/cdefs.h>
@@ -765,10 +765,10 @@ udf_root(struct mount *mp, int lktype, struct vnode **vpp)
 	if (error)
 		return error;
 
-	if (!root_dir)
-		error = ENOENT;
+	if (!root_dir) /* if root_dir is NULL? error = ENOENT */
+		error = ENOENT; /* shouldn be "return ENOENT" ? */
 
-	vp = root_dir->vnode;
+	vp = root_dir->vnode; /* goes ahead to then dereference root_dir */
 	KASSERT(vp->v_vflag & VV_ROOT);
 
 	*vpp = vp;
